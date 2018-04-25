821c6771ae198a20112a2d17895ede21b12ec092
Victor Utreras <vutreras@live.com>
2018-04-25 12:09:26 -0300

    Agrega logica para ejecucion de varios ambientes y api_host

--- app/README.md
+++ app/README.md
@@ -19,3 +19,35 @@
   Ejemplo: Ejecutar los test en el ambiente definido en el parametro "test"
 
   > ./test.sh -Denv=test
+
+## Parametros de test.sh
+
+  - env: OBLIGATORIO Ambiente test, development, production, jenkins u otro
+  - api_host: OPCIONAL: nombre del host donde se encuentra el api, por defecto es http://localhost
+  
+  ### IMPORTANTE:
+  
+  Los ambientes de ejecución definen los lugares donde se ejecuta el api, servidor y puerto
+  
+  - test: Ambientes test o test-* indican que el api se ejecutará en un servidor embebido con un puerto aleatorio
+  - jenkins: Ambiente jenkins indica que el api se ejecutará en un servidor embebido con un puerto aleatorio
+  - development: Ambientes development o development-* indican que el api se ejecuta en un servidor local o externo generalmente en el pueto 8080
+  - production: Ambientes production o production-* indican que el api se ejecuta en un servidor local o externo generalmente en el pueto 8080
+  
+  ### Ejemplos:
+  
+    Ejecuta los test en el ambiente "test", por defecto al "api_host" http://localhost + puerto aleatorio
+    
+      > ./test.sh -Denv=test
+    
+    Ejecuta los test en el ambiente "test", reescribiendo el "api_host" a http://127.0.0.1 + puerto aleatorio
+    
+      > ./test.sh -Denv=test -Dapi_host=http://127.0.0.1
+    
+    Ejecuta los test en el ambiente "development" por defecto apuntando el "api_host" a http://localhost:8080
+    
+      > ./test.sh -Denv=development
+    
+    Ejecuta los test en el ambiente "development", reescribiendo el "api_host" a http://otramaquina:9090
+          
+      > ./test.sh -Denv=development -Dapi_host=http://otramaquina:9090
--- app/src/test/java/cl/multicaja/prepago/test/api/TestServer.java
+++ app/src/test/java/cl/multicaja/prepago/test/api/TestServer.java
@@ -1,8 +1,11 @@
 package cl.multicaja.prepago.test.api;
 
 import cl.multicaja.prepago.test.TestApiBase;
+import cl.multicaja.prepago.utils.ConfigUtils;
 import cl.multicaja.prepago.utils.NumberUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.glassfish.embeddable.BootstrapProperties;
 import org.glassfish.embeddable.GlassFish;
 import org.glassfish.embeddable.GlassFishProperties;
@@ -17,6 +20,8 @@ import java.util.Arrays;
  */
 public class TestServer {
 
+  private static Log log = LogFactory.getLog(TestServer.class);
+
   private GlassFish glassfish;
 
   public TestServer() {
@@ -29,14 +34,41 @@ public class TestServer {
    */
   public void start() throws Exception {
 
+    //por defecto obliga a que lo que se ejecute mediante test use un BasicDatasource y no uno por jndi
+    System.setProperty("db.use.basicdatasource", "true");
+
+    //se genera un port aleatorio
     int port = NumberUtils.getInstance().random(3200, 7200);
     String env = System.getProperty("env");
 
-    System.out.println("Suite env: " + System.getProperty("env"));
-    System.out.println("Suite port: http: " + port + ", https: " + (port + 1));
-
     if (StringUtils.isBlank(env)) {
-      throw new IllegalArgumentException("Falta la definicion del parametro -Denv con los valores: test, development, production, jenkins");
+      throw new IllegalArgumentException("Falta la definicion del parametro env con los valores: test, development, production, jenkins");
+    }
+
+    //se establecen los datos de conexion http al TestApiBase
+    TestApiBase.PORT_HTTP = port;
+    TestApiBase.PORT_HTTPS = port + 1;
+    TestApiBase.CONTEXT_PATH = ConfigUtils.getInstance().getProperty("context.path");
+
+    //en caso que sea development* o production* lo puertos por defecto son 8080 y 8181 dado que son los puertos
+    //del servidor externo
+    if (env.startsWith("development") || env.startsWith("production")) {
+      TestApiBase.PORT_HTTP = 8080;
+      TestApiBase.PORT_HTTPS = 8181;
+    }
+
+    TestApiBase testApiBase = new TestApiBase();
+
+    log.info("============================================");
+    log.info("Suite env: " + env);
+    log.info("Suite host: " + testApiBase.getApiHost().get("host"));
+    log.info("Suite port: " + testApiBase.getApiHost().get("port"));
+    log.info("============================================");
+
+    //Si es development o production quiere decir que se intenta conectar a un payara externo, en este caso no es necesario
+    //ejecutar un payara embebido
+    if (env.startsWith("development") || env.startsWith("production")) {
+      return;
     }
 
     File dirWar = new File("./target");
@@ -48,7 +80,7 @@ public class TestServer {
       }
     });
 
-    System.out.println("Files war: " + Arrays.asList(files));
+    log.info("war: " + Arrays.asList(files));
 
     if (files == null || files.length == 0) {
       throw new RuntimeException("No existe el war para desplegar en el servidor de test");
@@ -58,15 +90,11 @@ public class TestServer {
 
     System.out.println("Se desplegara el war: " + fileWar.getAbsolutePath());
 
-    TestApiBase.PORT_HTTP = port;
-    TestApiBase.PORT_HTTPS = port + 1;
-    TestApiBase.CONTEXT_PATH = fileWar.getName().replace(".war", "");
-
     BootstrapProperties bootstrap = new BootstrapProperties();
     GlassFishRuntime runtime = GlassFishRuntime.bootstrap(bootstrap);
     GlassFishProperties glassfishProperties = new GlassFishProperties();
-    glassfishProperties.setPort("http-listener", port);
-    glassfishProperties.setPort("https-listener", port + 1);
+    glassfishProperties.setPort("http-listener", TestApiBase.PORT_HTTP);
+    glassfishProperties.setPort("https-listener", TestApiBase.PORT_HTTPS);
     glassfish = runtime.newGlassFish(glassfishProperties);
     glassfish.start();
     glassfish.getDeployer().deploy(fileWar);
