726b0e5ac4d87bef9d7fa9e2bb2b46e3bf062bb5
Victor Utreras <vutreras@live.com>
2018-05-24 10:14:40 -0400

    Se dejan en ingles los estados de la tarjeta

--- app/src/main/java/cl/multicaja/prepaid/model/v10/PrepaidCardStatus.java
+++ app/src/main/java/cl/multicaja/prepaid/model/v10/PrepaidCardStatus.java
@@ -1,27 +1,16 @@
 package cl.multicaja.prepaid.model.v10;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 import java.io.Serializable;
 
 /**
  * @author abarazarte
  */
 public enum PrepaidCardStatus implements Serializable {
-  ACTIVE("activa"),
-  LOCKED("bloqueada"),
-  EXPIRED("expirada");
 
-  private String value;
-
-  PrepaidCardStatus(String value) {
-    this.value = value;
-  }
-
-  @JsonValue
-  public String getValue() {
-    return value;
-  }
+  ACTIVE,
+  LOCKED,
+  LOCKED_HARD,
+  EXPIRED;
 
   public static PrepaidCardStatus valueOfEnum(String name) {
     try {
