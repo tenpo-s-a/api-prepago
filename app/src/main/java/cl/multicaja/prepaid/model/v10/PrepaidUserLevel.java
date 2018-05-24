925cbb88d44cfc7498d63c29d3e1306f3b2871b4
Victor Utreras <vutreras@live.com>
2018-05-24 09:32:25 -0400

    Refactor de código

--- app/src/main/java/cl/multicaja/prepaid/model/v10/PrepaidUserLevel.java
+++ app/src/main/java/cl/multicaja/prepaid/model/v10/PrepaidUserLevel.java
@@ -3,5 +3,13 @@ package cl.multicaja.prepaid.model.v10;
 public enum PrepaidUserLevel {
   LEVEL_1,
   LEVEL_2,
-  LEVEL_3
+  LEVEL_3;
+
+  public static PrepaidUserLevel valueOfEnum(String name) {
+    try {
+      return PrepaidUserLevel.valueOf(name);
+    } catch(Exception ex) {
+      return null;
+    }
+  }
 }
