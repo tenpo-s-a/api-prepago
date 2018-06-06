f610c503b8f1c68ae3f8e586414c8289a3eaf851
Victor Utreras <vutreras@live.com>
2018-06-06 17:15:46 -0400

    Agrega test de validacion de parametros

--- app/src/test/java/cl/multicaja/test/v10/unit/Test_PrepaidEJBBean10_withdrawalCalculator.java
+++ app/src/test/java/cl/multicaja/test/v10/unit/Test_PrepaidEJBBean10_withdrawalCalculator.java
@@ -1,9 +1,11 @@
 package cl.multicaja.test.v10.unit;
 
 
+import cl.multicaja.core.exceptions.ValidationException;
 import cl.multicaja.prepaid.model.v10.*;
 import cl.multicaja.tecnocom.constants.CodigoMoneda;
 import cl.multicaja.users.model.v10.User;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -12,6 +14,89 @@ import java.math.BigDecimal;
  * @autor vutreras
  */
 public class Test_PrepaidEJBBean10_withdrawalCalculator extends TestBaseUnit {
+
+  @Test
+  public void testCalculatorErrorParamsNull() throws Exception {
+
+    final Integer codErrorParamNull = 101004;
+
+    {
+      AmountAndCurrency10 amountAndCurrency10 = new AmountAndCurrency10();
+      amountAndCurrency10.setCurrencyCode(CodigoMoneda.CHILE_CLP);
+      amountAndCurrency10.setValue(BigDecimal.valueOf(3000));
+
+      CalculatorRequest10 calculatorRequest = new CalculatorRequest10();
+      calculatorRequest.setAmount(amountAndCurrency10);
+      calculatorRequest.setPaymentMethod(numberUtils.random() ? TransactionOriginType.WEB : TransactionOriginType.POS);
+      calculatorRequest.setUserRut(null);
+
+      try {
+        getPrepaidEJBBean10().withdrawalCalculator(null, calculatorRequest);
+      } catch(ValidationException vex) {
+        Assert.assertEquals("debe ser error de validacion de parametros", codErrorParamNull, vex.getCode());
+      }
+    }
+    {
+      AmountAndCurrency10 amountAndCurrency10 = new AmountAndCurrency10();
+      amountAndCurrency10.setCurrencyCode(CodigoMoneda.CHILE_CLP);
+      amountAndCurrency10.setValue(BigDecimal.valueOf(3000));
+
+      CalculatorRequest10 calculatorRequest = new CalculatorRequest10();
+      calculatorRequest.setAmount(amountAndCurrency10);
+      calculatorRequest.setPaymentMethod(null);
+      calculatorRequest.setUserRut(1);
+
+      try {
+        getPrepaidEJBBean10().withdrawalCalculator(null, calculatorRequest);
+      } catch(ValidationException vex) {
+        Assert.assertEquals("debe ser error de validacion de parametros", codErrorParamNull, vex.getCode());
+      }
+    }
+    {
+      CalculatorRequest10 calculatorRequest = new CalculatorRequest10();
+      calculatorRequest.setAmount(null);
+      calculatorRequest.setPaymentMethod(numberUtils.random() ? TransactionOriginType.WEB : TransactionOriginType.POS);
+      calculatorRequest.setUserRut(1);
+
+      try {
+        getPrepaidEJBBean10().withdrawalCalculator(null, calculatorRequest);
+      } catch(ValidationException vex) {
+        Assert.assertEquals("debe ser error de validacion de parametros", codErrorParamNull, vex.getCode());
+      }
+    }
+    {
+      AmountAndCurrency10 amountAndCurrency10 = new AmountAndCurrency10();
+      amountAndCurrency10.setCurrencyCode(CodigoMoneda.CHILE_CLP);
+      amountAndCurrency10.setValue(null);
+
+      CalculatorRequest10 calculatorRequest = new CalculatorRequest10();
+      calculatorRequest.setAmount(amountAndCurrency10);
+      calculatorRequest.setPaymentMethod(numberUtils.random() ? TransactionOriginType.WEB : TransactionOriginType.POS);
+      calculatorRequest.setUserRut(null);
+
+      try {
+        getPrepaidEJBBean10().withdrawalCalculator(null, calculatorRequest);
+      } catch(ValidationException vex) {
+        Assert.assertEquals("debe ser error de validacion de parametros", codErrorParamNull, vex.getCode());
+      }
+    }
+    {
+      AmountAndCurrency10 amountAndCurrency10 = new AmountAndCurrency10();
+      amountAndCurrency10.setCurrencyCode(null);
+      amountAndCurrency10.setValue(BigDecimal.valueOf(3000));
+
+      CalculatorRequest10 calculatorRequest = new CalculatorRequest10();
+      calculatorRequest.setAmount(amountAndCurrency10);
+      calculatorRequest.setPaymentMethod(numberUtils.random() ? TransactionOriginType.WEB : TransactionOriginType.POS);
+      calculatorRequest.setUserRut(null);
+
+      try {
+        getPrepaidEJBBean10().withdrawalCalculator(null, calculatorRequest);
+      } catch(ValidationException vex) {
+        Assert.assertEquals("debe ser error de validacion de parametros", codErrorParamNull, vex.getCode());
+      }
+    }
+  }
 
   @Test
   public void testCalculatorOk() throws Exception {
@@ -41,5 +126,4 @@ public class Test_PrepaidEJBBean10_withdrawalCalculator extends TestBaseUnit {
 
     //TODO falta completar el test
   }
-
 }
