702e7224358acf44dfe52eba9d35b6cf18949774
Victor Utreras <vutreras@live.com>
2018-06-07 15:36:46 -0400

    Correccion de rutas

--- kong/src/test/java/cl/multicaja/test/kong/Test_20180420120200.java
+++ kong/src/test/java/cl/multicaja/test/kong/Test_20180420120200.java
@@ -10,7 +10,7 @@ public class Test_20180420120200 extends TestKongBase {
   @Test
   public void ping_1_0() {
 
-    HttpResponse resp = kongGET("/prepaid/ping", "api-prepaid-1.0");
+    HttpResponse resp = kongGET("/prepaid/prepaid/ping", "api-prepaid-1.0");
 
     Assert.assertEquals("status 200", 200, resp.getStatus());
     Assert.assertEquals("servicio 1.0", "PrepaidResource10", resp.toMap().get("service"));
