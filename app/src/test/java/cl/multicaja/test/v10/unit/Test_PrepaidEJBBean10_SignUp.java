16707f2961182ca553bc9e320f4d71747715bc08
Jonathan Orellana <orellana.gourdet@gmail.com>
2018-06-28 18:31:19 -0400

    signup

--- app/src/test/java/cl/multicaja/test/v10/unit/Test_PrepaidEJBBean10_SignUp.java
+++ app/src/test/java/cl/multicaja/test/v10/unit/Test_PrepaidEJBBean10_SignUp.java
@@ -3,13 +3,14 @@ package cl.multicaja.test.v10.unit;
 import cl.multicaja.core.exceptions.BadRequestException;
 import cl.multicaja.core.exceptions.ValidationException;
 import cl.multicaja.prepaid.model.v10.NewPrepaidUserSignup10;
+import cl.multicaja.prepaid.model.v10.PrepaidUserSignup10;
 import cl.multicaja.users.model.v10.User;
 import cl.multicaja.users.model.v10.UserStatus;
 import org.junit.Assert;
 import org.junit.Test;
 
 import static cl.multicaja.core.model.Errors.CLIENTE_BLOQUEADO_O_BORRADO;
-import static cl.multicaja.core.model.Errors.CLIENTE_YA_TIENE_CLAVE;
+import static cl.multicaja.core.model.Errors.CORREO_YA_UTILIZADO;
 
 public class Test_PrepaidEJBBean10_SignUp extends TestBaseUnit {
 
@@ -40,15 +41,13 @@ public class Test_PrepaidEJBBean10_SignUp extends TestBaseUnit {
         NewPrepaidUserSignup10 newPrepaidUserSignup10 = new NewPrepaidUserSignup10();
         newPrepaidUserSignup10.setRut(user.getRut().getValue());
         newPrepaidUserSignup10.setEmail(user.getEmail().getValue());
-        getPrepaidEJBBean10().initUserSignup(null, newPrepaidUserSignup10);
-
-      } catch (ValidationException e) {
-        Assert.assertEquals("Error Usuario ya existe", CLIENTE_YA_TIENE_CLAVE.getValue(), e.getCode());
+        PrepaidUserSignup10 prepaidUserSignup10 = getPrepaidEJBBean10().initUserSignup(null, newPrepaidUserSignup10);
+        Assert.assertNotNull("Debe existir prepaid user signup",prepaidUserSignup10);
+        System.out.println("CLIENTE SIN PROBLEMAS");
       } catch (Exception e) {
         Assert.fail("No debe llegar aca !!ERROR!!");
       }
     }
-
     {// USUARIO BLOQUEADO
       try
       {
@@ -58,12 +57,27 @@ public class Test_PrepaidEJBBean10_SignUp extends TestBaseUnit {
         newPrepaidUserSignup10.setEmail(user.getEmail().getValue());
         getPrepaidEJBBean10().initUserSignup(null, newPrepaidUserSignup10);
       } catch (ValidationException e) {
+        System.out.println("CLIENTE_BLOQUEADO_O_BORRADO");
         Assert.assertEquals("Error Usuario ya existe", CLIENTE_BLOQUEADO_O_BORRADO.getValue(), e.getCode());
       } catch (Exception e) {
         Assert.fail("No debe llegar aca !!ERROR!!");
       }
     }
 
-
+    {// USUARIO CON MAILS INCORRECTO
+      try
+      {
+        User user = registerUser(UserStatus.DELETED);
+        NewPrepaidUserSignup10 newPrepaidUserSignup10 = new NewPrepaidUserSignup10();
+        newPrepaidUserSignup10.setRut(getUniqueRutNumber());
+        newPrepaidUserSignup10.setEmail(user.getEmail().getValue());
+        getPrepaidEJBBean10().initUserSignup(null, newPrepaidUserSignup10);
+      } catch (ValidationException e) {
+        System.out.println("CORREO_YA_UTILIZADO");
+        Assert.assertEquals("Error Usuario ya existe", CORREO_YA_UTILIZADO.getValue(), e.getCode());
+      } catch (Exception e) {
+        Assert.fail("No debe llegar aca !!ERROR!!");
+      }
+    }
   }
 }
