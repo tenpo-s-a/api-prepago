2b516c8f704a19623538de80996ea21208823205
Victor Utreras <vutreras@live.com>
2018-07-09 20:30:26 -0400

    Cambio en implementacion de createPrepaidUser

--- app/src/main/java/cl/multicaja/prepaid/resources/v10/TestHelpersResource10.java
+++ app/src/main/java/cl/multicaja/prepaid/resources/v10/TestHelpersResource10.java
@@ -217,28 +217,45 @@ public final class TestHelpersResource10 extends BaseResource {
   }
 
   @POST
-  @Path("/prepaiduser/{userId}")
-  public Response createPrepaidUser(@PathParam("userId") Long userIdMc, @Context HttpHeaders headers) throws Exception {
+  @Path("/prepaiduser")
+  public Response createPrepaidUser(User user, @Context HttpHeaders headers) throws Exception {
 
     validate();
 
     Map<String, Object> mapHeaders = headersToMap(headers);
 
-    User user = usersEJBBean10.getUserById(mapHeaders, userIdMc);
+    if (user.getId() != null) {
 
-    if(user == null){
+      user = usersEJBBean10.getUserById(mapHeaders, user.getId());
+
+    } else {
+
+      SignUp signUp = usersEJBBean10.signUpUser(mapHeaders, user.getRut().getValue(), user.getEmail().getValue());
+
+      user = usersEJBBean10.getUserById(mapHeaders, signUp.getUserId());
+    }
+
+    if (user == null) {
       throw new NotFoundException(CLIENTE_NO_EXISTE);
     }
 
-    user = usersEJBBean10.fillUser(user);
+    if (StringUtils.isBlank(user.getName())) {
+      user.setName(null);
+    }
+    if (StringUtils.isBlank(user.getLastname_1())) {
+      user.setLastname_1(null);
+    }
+    if (StringUtils.isBlank(user.getLastname_2())) {
+      user.setLastname_2(null);
+    }
 
     user.setGlobalStatus(UserStatus.ENABLED);
     user.getRut().setStatus(RutStatus.VERIFIED);
     user.getEmail().setStatus(EmailStatus.VERIFIED);
     user.setNameStatus(NameStatus.VERIFIED);
-    user.setPassword(String.valueOf(numberUtils.random(1111,9999)));
+    user.setPassword(String.valueOf(1357));
 
-    user = usersEJBBean10.updateUser(user, user.getRut(), user.getEmail(), user.getCellphone(), user.getNameStatus(), user.getGlobalStatus(), user.getBirthday(), user.getPassword(), user.getCompanyData());
+    user = usersEJBBean10.fillUser(user);
 
     PrepaidUser10 prepaidUser = new PrepaidUser10();
     prepaidUser.setUserIdMc(user.getId());
@@ -246,14 +263,14 @@ public final class TestHelpersResource10 extends BaseResource {
     prepaidUser.setStatus(PrepaidUserStatus.ACTIVE);
     prepaidUser.setBalanceExpiration(0L);
 
-    prepaidUser = prepaidUserEJBBean10.createPrepaidUser(mapHeaders, prepaidUser);
+    prepaidUserEJBBean10.createPrepaidUser(mapHeaders, prepaidUser);
 
-    return Response.ok(prepaidUser).status(200).build();
+    return Response.ok(user).status(200).build();
   }
 
   @GET
-  @Path("/user/{userId}/mail_code")
-  public Response createUser(@PathParam("userId") Long userIdMc, @Context HttpHeaders headers) throws Exception {
+  @Path("/user/{userId}/email_code")
+  public Response getEmailCode(@PathParam("userId") Long userIdMc, @Context HttpHeaders headers) throws Exception {
 
     validate();
 
