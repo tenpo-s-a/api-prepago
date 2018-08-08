f42af7a010dcc3501ed98b67a2401cad908e2154
Gonzalo Salas <go.salass@gmail.com>
2018-08-08 15:47:02 -0400

    Correo error datos tarjeta
    Implementa nuevo template de error para cuando exista error en la consulta de datos de tarjeta en tecnocom

--- app/src/main/java/cl/multicaja/prepaid/model/v10/MailTemplates.java
+++ app/src/main/java/cl/multicaja/prepaid/model/v10/MailTemplates.java
@@ -10,6 +10,7 @@ public class MailTemplates {
   public static final String TEMPLATE_MAIL_EMISSION_ERROR = "Prepago/ErrorAltaRapida";
   public static final String TEMPLATE_MAIL_ERROR_TOPUP_RETURN = "Prepago/ErrorDevolucion";
   public static final String TEMPLATE_MAIL_ERROR_ISSUANCE_FEE = "Prepago/ErrorCobroEmision";
+  public static final String TEMPLATE_MAIL_ERROR_CREATE_CARD = "Prepago/ErrorDatosTarjeta";
 
   public static final String TEMPLATE_PDF_CARD = "card_pdf";//Template para el PDF
   public static final String TEMPLATE_MAIL_TOPUP = String.format("%s/RecepcionCarga", APPNAME);
