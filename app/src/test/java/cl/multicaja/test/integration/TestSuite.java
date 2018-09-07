4dbf9f9f43d05bd0531278d990f86ee6f8def9f1
Alejandro Barazarte <alejandro.barazarte@multicaja.cl>
2018-09-07 15:32:42 -0300

    Verifica conexion con api-users antes de correr tests de integracion

--- app/src/test/java/cl/multicaja/test/integration/TestSuite.java
+++ app/src/test/java/cl/multicaja/test/integration/TestSuite.java
@@ -1,6 +1,11 @@
 package cl.multicaja.test.integration;
 
+import cl.multicaja.core.test.TestApiBase;
 import cl.multicaja.core.test.TestSuiteBase;
+import cl.multicaja.core.utils.ConfigUtils;
+import cl.multicaja.core.utils.http.HttpError;
+import cl.multicaja.core.utils.http.HttpResponse;
+import cl.multicaja.core.utils.http.HttpUtils;
 import cl.multicaja.test.integration.v10.helper.sftp.TestSftpServer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,6 +24,10 @@ public class TestSuite extends TestSuiteBase {
   private static TestServer testServer = new TestServer();
 
   private static boolean runningInTestSuite = false;
+
+  private static HttpUtils httpUtils = HttpUtils.getInstance();
+
+  private static ConfigUtils configUtils = ConfigUtils.getInstance();
 
   /**
    *
@@ -67,13 +76,22 @@ public class TestSuite extends TestSuiteBase {
   }
 
   public static Class<?>[] suite() throws Exception {
-    String packageName = TestSuite.class.getPackage().getName();
-    log.info("packageName: " + packageName);
-    Class[] classList = getClasses(packageName);
-    log.info("------------ Lista de clases de test ------------");
-    for (Class cls : classList) {
-      log.info(cls.getSimpleName());
-    }
-    return  classList;
+
+      // Se verifica la conexion con api-users
+      String url = configUtils.getProperty("apis.user.url");
+      HttpResponse httpResponse = httpUtils.get(String.format("%s/ping", url));
+      if(HttpError.TIMEOUT_CONNECTION.equals(httpResponse.getHttpError()) || HttpError.TIMEOUT_RESPONSE.equals(httpResponse.getHttpError())){
+        throw new IllegalStateException("Sin conexion con api-users");
+      }
+
+
+      String packageName = TestSuite.class.getPackage().getName();
+      log.info("packageName: " + packageName);
+      Class[] classList = getClasses(packageName);
+      log.info("------------ Lista de clases de test ------------");
+      for (Class cls : classList) {
+        log.info(cls.getSimpleName());
+      }
+      return  classList;
   }
 }
