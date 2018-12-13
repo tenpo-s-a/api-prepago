a3cdeb4df7db933c0fe4c9afe5d446a0696d8d69
Victor  Godoy Hernández <vigohe@gmail.com>
2018-12-13 16:03:02 -0300

    upgrade: multicaja-base from nexus

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 cd ./libs
-jar xf multicaja-base-1.0.0.jar META-INF/maven/cl.multicaja/multicaja-base/pom.xml
-mv  META-INF/maven/cl.multicaja/multicaja-base/pom.xml .
-rm -rf META-INF
-cd ..
-mvn install:install-file -Dfile=./libs/multicaja-base-1.0.0.jar -DgroupId=cl.multicaja -DartifactId=multicaja-base -Dversion=1.0.0 -Dpackaging=jar -DpomFile=./libs/pom.xml
-rm -rf ./libs/pom.xml
-
-cd ./libs
 jar xf multicaja-async-1.0.0.jar META-INF/maven/cl.multicaja/multicaja-async/pom.xml
 mv  META-INF/maven/cl.multicaja/multicaja-async/pom.xml .
 rm -rf META-INF
