c5a7d5ed17898f2b087395ea3834c5c9b0a90096
Alejandro Barazarte <alejandro.barazarte@multicaja.cl>
2018-12-18 14:49:25 -0300

    Agrega tabla para almacenar informacion sobre los archivos ipm procesados

---
+++ pg/src/test/java/cl/multicaja/test/db/Test_20181218133602_create_table_acc_ipm_file.java
@@ -0,0 +1,28 @@
+package cl.multicaja.test.db;
+
+import cl.multicaja.core.utils.db.ColumnInfo;
+import cl.multicaja.core.utils.db.SqlType;
+import cl.multicaja.test.TestDbBasePg;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test_20181218133602_create_table_acc_ipm_file  extends TestDbBasePg {
+
+  @Test
+  public void checkIfExistsTable_ipm_file() {
+
+
+    boolean exists = dbUtils.tableExists(SCHEMA_ACCOUNTING, "ipm_file", true,
+
+      new ColumnInfo("id", SqlType.BIGSERIAL.getGetJavaType()),
+      new ColumnInfo("file_name", SqlType.VARCHAR.getGetJavaType(), 100),
+      new ColumnInfo("file_id",SqlType.VARCHAR.getGetJavaType(), 25),
+      new ColumnInfo("message_count", SqlType.NUMERIC.getGetJavaType(), 15),
+      new ColumnInfo("status", SqlType.VARCHAR.getGetJavaType(), 25),
+      new ColumnInfo("create_date",SqlType.TIMESTAMP.getGetJavaType()),
+      new ColumnInfo("update_date", SqlType.TIMESTAMP.getGetJavaType())
+    );
+
+    Assert.assertTrue("Existe tabla accounting", exists);
+  }
+}
