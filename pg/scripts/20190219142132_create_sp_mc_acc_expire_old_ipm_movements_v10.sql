bca4d484b8d476ecdafa65c467dba91eb3ca9b3e
Agustin Antonissen <agustin.antonissen@multicaja.cl>
2019-02-19 17:15:43 -0300

    Agrega un SP para expirar movimientos de compra y subscripcion

---
+++ pg/scripts/20190219142132_create_sp_mc_acc_expire_old_ipm_movements_v10.sql
@@ -0,0 +1,43 @@
+--
+--    Copyright 2010-2016 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+-- // create_sp_mc_acc_expire_old_ipm_movements_v10
+-- Migration SQL that makes the change goes here.
+
+CREATE OR REPLACE FUNCTION ${schema.acc}.create_sp_mc_acc_expire_old_ipm_movements_v10()
+RETURNS VOID
+AS $$
+BEGIN
+
+UPDATE
+  ${schema}.prp_movimiento mov
+SET
+  estado = 'EXPIRED'
+WHERE
+  mov.estado = 'PENDING' AND
+  (mov.tipo_movimiento = 'SUBSCRIPTION' OR mov.tipo_movimiento = 'PURCHASE') AND
+  (SELECT COUNT(f.id)
+   FROM ${schema.acc}.ipm_file f
+   WHERE f.create_date >= mov.fecha_creacion) >= 7;
+
+RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP FUNCTION IF EXISTS ${schema.acc}.create_sp_mc_acc_expire_old_ipm_movements_v10();
---
+++ pg/src/test/java/cl/multicaja/test/db/Test_20190219142132_create_sp_mc_acc_expire_old_ipm_movements_v10.java
@@ -0,0 +1,121 @@
+package cl.multicaja.test.db;
+
+import cl.multicaja.test.TestDbBasePg;
+import org.junit.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Test_20190219142132_create_sp_mc_acc_expire_old_ipm_movements_v10 extends TestDbBasePg {
+  private static final String SP_NAME = SCHEMA_ACCOUNTING + ".create_sp_mc_acc_expire_old_ipm_movements_v10";
+
+  @Before
+  @After
+  public void beforeClass() {
+    dbUtils.getJdbcTemplate().execute(String.format("truncate %s.ipm_file cascade", SCHEMA_ACCOUNTING));
+    dbUtils.getJdbcTemplate().execute(String.format("truncate %s.prp_movimiento cascade", SCHEMA));
+  }
+
+  @Test
+  public void expireOldSubscriptionMovements() throws SQLException, InterruptedException {
+
+    // Preparar usuario
+    Map<String, Object> mapCard = Test_20180514105345_create_sp_mc_prp_crear_tarjeta_v10.insertCard("ACTIVA");
+    Long idMovimientoRef = getUniqueLong();
+    Long idUsuario = (Long)mapCard.get("id_usuario");
+
+    ArrayList<Map<String, Object>> allMovements = new ArrayList<>();
+    for(int i = 0; i < 10; i++) {
+      Test_20181218135154_create_sp_mc_acc_create_ipm_file_v10.createIpmFile(String.format("FileName%d", i), "FileId", 10, "Status");
+      Map<String, Object> newMovement = Test_20180523092338_create_sp_mc_prp_crea_movimiento_v10.insertMovement(idMovimientoRef, idUsuario, getUniqueLong().toString(), "SUBSCRIPTION", "PENDING", "API", getRandomNumericString(10), 152, 0, 3001);
+      allMovements.add(0, newMovement);
+      Thread.sleep(10);
+    }
+
+    dbUtils.execute(SP_NAME);
+
+    for(int i = 0; i < 10; i++) {
+      Map<String, Object> movement = allMovements.get(i);
+      Map<String, Object> storedMovement = getMovement(numberUtils.toLong(movement.get("_id")));
+      Assert.assertEquals("Debe tener el mismo id", movement.get("_id").toString(), storedMovement.get("id").toString());
+
+      if(i <= 6) {
+        Assert.assertEquals("Los primeros deben tener el estado PENDING", "PENDING", storedMovement.get("estado").toString());
+      } else {
+        System.out.println("idx: " + i);
+        Assert.assertEquals("Del 7 en adelante deben tener el estado EXPIRED", "EXPIRED", storedMovement.get("estado").toString());
+      }
+    }
+  }
+
+  @Test
+  public void expireOldPurchaseMovements() throws SQLException, InterruptedException {
+
+    // Preparar usuario
+    Map<String, Object> mapCard = Test_20180514105345_create_sp_mc_prp_crear_tarjeta_v10.insertCard("ACTIVA");
+    Long idMovimientoRef = getUniqueLong();
+    Long idUsuario = (Long)mapCard.get("id_usuario");
+
+    ArrayList<Map<String, Object>> allMovements = new ArrayList<>();
+    for(int i = 0; i < 10; i++) {
+      Test_20181218135154_create_sp_mc_acc_create_ipm_file_v10.createIpmFile(String.format("FileName%d", i), "FileId", 10, "Status");
+      Map<String, Object> newMovement = Test_20180523092338_create_sp_mc_prp_crea_movimiento_v10.insertMovement(idMovimientoRef, idUsuario, getUniqueLong().toString(), "PURCHASE", "PENDING", "API", getRandomNumericString(10), 152, 0, 3001);
+      allMovements.add(0, newMovement);
+      Thread.sleep(10);
+    }
+
+    dbUtils.execute(SP_NAME);
+
+    for(int i = 0; i < 10; i++) {
+      Map<String, Object> movement = allMovements.get(i);
+      Map<String, Object> storedMovement = getMovement(numberUtils.toLong(movement.get("_id")));
+      Assert.assertEquals("Debe tener el mismo id", movement.get("_id").toString(), storedMovement.get("id").toString());
+
+      if(i <= 6) {
+        Assert.assertEquals("Los primeros deben tener el estado PENDING", "PENDING", storedMovement.get("estado").toString());
+      } else {
+        Assert.assertEquals("Del 7 en adelante deben tener el estado EXPIRED", "EXPIRED", storedMovement.get("estado").toString());
+      }
+    }
+  }
+
+  @Test
+  public void doNOTexpireOldAnyMovements() throws SQLException, InterruptedException {
+
+    // Preparar usuario
+    Map<String, Object> mapCard = Test_20180514105345_create_sp_mc_prp_crear_tarjeta_v10.insertCard("ACTIVA");
+    Long idMovimientoRef = getUniqueLong();
+    Long idUsuario = (Long)mapCard.get("id_usuario");
+
+    ArrayList<Map<String, Object>> allMovements = new ArrayList<>();
+    for(int i = 0; i < 10; i++) {
+      Test_20181218135154_create_sp_mc_acc_create_ipm_file_v10.createIpmFile(String.format("FileName%d", i), "FileId", 10, "Status");
+      Map<String, Object> newMovement = Test_20180523092338_create_sp_mc_prp_crea_movimiento_v10.insertMovement(idMovimientoRef, idUsuario, getUniqueLong().toString(), getRandomString(8), "PENDING", "API", getRandomNumericString(10), 152, 0, 3001);
+      allMovements.add(0, newMovement);
+      Thread.sleep(10);
+    }
+
+    dbUtils.execute(SP_NAME);
+
+    for(int i = 0; i < 10; i++) {
+      Map<String, Object> movement = allMovements.get(i);
+      Map<String, Object> storedMovement = getMovement(numberUtils.toLong(movement.get("_id")));
+      Assert.assertEquals("Debe tener el mismo id", movement.get("_id").toString(), storedMovement.get("id").toString());
+      Assert.assertEquals("Todos deben tener el estado PENDING", "PENDING", storedMovement.get("estado").toString());
+    }
+  }
+
+  private Map<String, Object> getMovement(Long id) {
+    return dbUtils.getJdbcTemplate().queryForList(
+      " SELECT " +
+        "     id, " +
+        "     estado " +
+        " FROM " +
+        "   "+ SCHEMA +".prp_movimiento"+
+        " WHERE " +
+        " id = " + id
+    ).get(0);
+  }
+}
