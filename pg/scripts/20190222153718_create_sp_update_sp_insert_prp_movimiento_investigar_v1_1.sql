c18355dfa007c2192af09fbac0400d2c712b6e7e
mauricio-valencia <mauricio.valencia@multicaja.cl>
2019-02-23 20:01:04 -0300

    Change: Cambios en Tabla archivo_investigacion y sps de inserción y busqueda

---
+++ pg/scripts/20190222153718_create_sp_update_sp_insert_prp_movimiento_investigar_v1_1.sql
@@ -0,0 +1,134 @@
+--
+--    Copyright 2010-2016 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+-- // create_sp_update_sp_insert_prp_movimiento_investigar_v1_1
+-- Migration SQL that makes the change goes here.
+
+CREATE OR REPLACE FUNCTION ${schema}.mc_prp_crea_movimiento_investigar_v1_1(
+	IN _id_archivo_origen	    VARCHAR,
+	IN _origen			          VARCHAR,
+	IN _nombre_archivo	      VARCHAR,
+	IN _fecha_de_transaccion  TIMESTAMP,
+	IN _responsable           VARCHAR,
+	IN _descripcion           VARCHAR,
+	IN _mov_ref               BIGINT,
+	OUT _error_code		        VARCHAR,
+	OUT _error_msg		        VARCHAR
+)AS $$
+
+BEGIN
+	_error_code := '0';
+	_error_msg := '';
+
+IF
+COALESCE(_id_archivo_origen,'') = '' AND
+COALESCE(_origen,'') = '' AND
+COALESCE(_nombre_archivo,'') = '' AND
+COALESCE(_fecha_de_transaccion,to_timestamp(0)) = to_timestamp(0) AND
+COALESCE(_responsable,'') = '' AND
+COALESCE(_descripcion,'') = '' AND
+COALESCE(_mov_ref,0) = 0
+THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] id_archivo_origen, origen, nombre_archivo, fecha_de_transaccion, responsable, descripcion y mov_ref son campos obligatorios';
+RETURN;
+END IF;
+
+IF COALESCE(_id_archivo_origen,'') = '' THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] El id_archivo_origen es obligatorio';
+RETURN;
+END IF;
+
+IF COALESCE(_origen,'') = '' THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] El origen es obligatorio';
+RETURN;
+END IF;
+
+IF COALESCE(_nombre_archivo,'') = '' THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] El nombre_archivo es obligatorio';
+RETURN;
+END IF;
+
+IF COALESCE(_fecha_de_transaccion,to_timestamp(0)) = to_timestamp(0) THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] La fecha_de_transaccion es obligatoria';
+RETURN;
+END IF;
+
+IF COALESCE(_responsable,'') = '' THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] El responsable es obligatorio';
+RETURN;
+END IF;
+
+IF COALESCE(_descripcion,'') = '' THEN
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] La descripcion es obligatoria';
+RETURN;
+END IF;
+
+INSERT INTO ${schema}.prp_movimiento_investigar
+(
+id_archivo_origen,
+origen,
+nombre_archivo,
+fecha_registro,
+fecha_de_transaccion,
+responsable,
+descripcion,
+mov_ref
+)
+VALUES
+(
+_id_archivo_origen,
+_origen,
+coalesce(_nombre_archivo,''),
+timezone('utc', now()),
+_fecha_de_transaccion,
+_responsable,
+_descripcion,
+_mov_ref
+);
+
+EXCEPTION WHEN OTHERS THEN
+
+IF COALESCE(_mov_ref,cast(_mov_ref as bigint)) = cast(_mov_ref as bigint) THEN
+RETURN;
+ELSE
+_error_code := '101000';
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] El mov_ref es obligatorio';
+RETURN;
+END IF;
+
+IF COALESCE(_mov_ref,NOT NULL) = NOT NULL THEN
+_error_code := SQLSTATE;
+_error_msg := '[mc_prp_crea_movimiento_investigar_v1_1] Error al insertar Movimiento conciliado. CAUSA ('|| SQLERRM ||')';
+RETURN;
+END IF;
+
+END;
+$$ LANGUAGE plpgsql;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP FUNCTION IF EXISTS ${schema}.mc_prp_crea_movimiento_investigar_v1_1(VARCHAR,VARCHAR,VARCHAR,TIMESTAMP,VARCHAR,VARCHAR,BIGINT);
+
+
+
--- pg/src/test/java/cl/multicaja/test/db/Test_20181010114034_create_sp_busca_movimientos_a_investigar.java
+++ pg/src/test/java/cl/multicaja/test/db/Test_20181010114034_create_sp_busca_movimientos_a_investigar.java
@@ -1,12 +1,8 @@
 package cl.multicaja.test.db;
 
 import cl.multicaja.core.utils.db.InParam;
-import cl.multicaja.core.utils.db.NullParam;
-import cl.multicaja.core.utils.db.OutParam;
 import cl.multicaja.core.utils.db.RowMapper;
 import cl.multicaja.test.TestDbBasePg;
-import org.apache.commons.lang3.RandomUtils;
-import org.apache.ibatis.jdbc.Null;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,11 +11,12 @@ import org.junit.Test;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public class Test_20181010114034_create_sp_busca_movimientos_a_investigar extends TestDbBasePg {
-  private static final String SP_NAME = SCHEMA + ".mc_prp_busca_movimientos_a_investigar_v10";
+  private static final String SP_NAME = SCHEMA + ".mc_prp_busca_movimientos_a_investigar_v1_1";
 
   @Before
   public void before() {
@@ -31,97 +28,123 @@ public class Test_20181010114034_create_sp_busca_movimientos_a_investigar extend
     dbUtils.getJdbcTemplate().execute(String.format("delete from %s.prp_movimiento_investigar", SCHEMA));
   }
 
-  @Test
-  public void testBuscaMovimientosAInvestigar_idOk() throws SQLException {
-
-    Test_20181009113614_create_sp_crea_movimiento_investigar.creaMovimientoInvestigar("idMov=48", "Testing", "archivo.txt");
+  public Map<String, Object> searchMovimientoInvestigar(String idArchivoOrigen) throws SQLException {
 
     Object[] params = {
-      new InParam("idMov=48", Types.VARCHAR)
+      new InParam(idArchivoOrigen, Types.VARCHAR)
     };
 
     RowMapper rm = (Map<String, Object> row) -> {
       ResearchMovement researchMovement = new ResearchMovement();
       researchMovement.id = numberUtils.toLong(row.get("_id"));
-      researchMovement.idRef = String.valueOf(row.get("_mov_ref"));
-      researchMovement.origin = String.valueOf(row.get("_origen"));
-      researchMovement.fileName = String.valueOf(row.get("_nombre_archivo"));
-      researchMovement.createdAt = (Timestamp) row.get("_fecha_registro");
+      //researchMovement.idRef = String.valueOf(row.get("_mov_ref"));
+      researchMovement.idArchivoOrigen = String.valueOf(row.get("_id_archivo_origen"));
+      researchMovement.origen = String.valueOf(row.get("_origen"));
+      researchMovement.nombreArchivo = String.valueOf(row.get("_nombre_archivo"));
+      researchMovement.fechaRegistro = (Timestamp) row.get("_fecha_registro");
+      researchMovement.fechaDeTransaccion = (Timestamp) row.get("_fecha_de_transaccion");
+      researchMovement.responsable = String.valueOf(row.get("_responsable"));
+      researchMovement.descripcion = String.valueOf(row.get("_descripcion"));
+      researchMovement.movRef = (long) row.get("_mov_ref");
+
       return researchMovement;
     };
 
-    Map<String, Object> resp = dbUtils.execute(SP_NAME, rm, params);
+    return dbUtils.execute(SP_NAME, rm, params);
+  }
 
+  @Test
+  public void testBuscaMovimientosAInvestigar_idOk() throws SQLException {
+
+    String idArchivoOrigen = "idMov=48";//+ getRandomString(10);
+    String origen = "Testing";
+    String nombreArchivo = "archivo.txt";
+    Timestamp fechaDeTransaccion = new Timestamp((new Date()).getTime());
+    String responsable = "Conciliaciones Prepago";
+    String descripcion = "Movimiento no encontrado en archivo";
+    Long movRef = new Long(0);
+
+    Test_20181009113614_create_sp_crea_movimiento_investigar.setMovimientoInvestigar(
+      idArchivoOrigen, origen,nombreArchivo,fechaDeTransaccion,responsable,descripcion,movRef);
+
+    Map<String, Object> resp = searchMovimientoInvestigar(idArchivoOrigen);
     List results = (List)resp.get("result");
     Assert.assertNotNull("Debe existir", results);
     Assert.assertEquals("Deben encontrar 1 movimiento", 1, results.size());
 
     ResearchMovement researchMovement = (ResearchMovement) results.get(0);
-    Assert.assertEquals("Debe tener el mismo mov ref", "idMov=48", researchMovement.idRef);
-    Assert.assertEquals("Debe tener mismo origen", "Testing", researchMovement.origin);
-    Assert.assertEquals("Debe tener el mismo archivo", "archivo.txt", researchMovement.fileName);
-    Assert.assertNotNull("Debe tener una fecha de creacion", researchMovement.createdAt);
+    Assert.assertEquals("Deben ser iguales",idArchivoOrigen,researchMovement.idArchivoOrigen);
+    Assert.assertEquals("Deben ser iguales",origen,researchMovement.origen);
+    Assert.assertEquals("Deben ser iguales",nombreArchivo,researchMovement.nombreArchivo);
+    Assert.assertEquals("Deben ser iguales",fechaDeTransaccion,researchMovement.fechaDeTransaccion);
+    Assert.assertEquals("Deben ser iguales",responsable,researchMovement.responsable);
+    Assert.assertEquals("Deben ser iguales",descripcion,researchMovement.descripcion);
+    Assert.assertEquals("Deben ser iguales",movRef,researchMovement.movRef);
+
   }
 
   @Test
   public void testBuscaMovimientosAInvestigar_idNull() throws SQLException {
 
-    Test_20181009113614_create_sp_crea_movimiento_investigar.creaMovimientoInvestigar("idMov=48", "Testing", "archivo1.txt");
-    Test_20181009113614_create_sp_crea_movimiento_investigar.creaMovimientoInvestigar("idMov=49", "Testong", "archivo2.txt");
-    Test_20181009113614_create_sp_crea_movimiento_investigar.creaMovimientoInvestigar("idMov=50", "Testung", "archivo3.txt");
+    String idArchivoOrigen1 = "idMov=48";//+ getRandomString(10);
+    String idArchivoOrigen2 = "idMov=49";//+ getRandomString(10);
+    String idArchivoOrigen3 = "idMov=50";//+ getRandomString(10);
+    String idArchivoOrigenNull = null;
+    String origen = "Testing";
+    String nombreArchivo = "archivo.txt";
+    Timestamp fechaDeTransaccion = new Timestamp((new Date()).getTime());
+    String responsable = "Conciliaciones Prepago";
+    String descripcion = "Movimiento no encontrado en archivo";
+    Long movRef = new Long(0);
 
-    Object[] params = {
-      new NullParam(Types.VARCHAR)
-    };
 
-    RowMapper rm = (Map<String, Object> row) -> {
-      ResearchMovement researchMovement = new ResearchMovement();
-      researchMovement.id = numberUtils.toLong(row.get("_id"));
-      researchMovement.idRef = String.valueOf(row.get("_mov_ref"));
-      researchMovement.origin = String.valueOf(row.get("_origen"));
-      researchMovement.fileName = String.valueOf(row.get("_nombre_archivo"));
-      researchMovement.createdAt = (Timestamp) row.get("_fecha_registro");
-      return researchMovement;
-    };
+    Test_20181009113614_create_sp_crea_movimiento_investigar.setMovimientoInvestigar(
+      idArchivoOrigen1, origen,nombreArchivo,fechaDeTransaccion,responsable,descripcion,movRef);
 
-    Map<String, Object> resp = dbUtils.execute(SP_NAME, rm, params);
+    Test_20181009113614_create_sp_crea_movimiento_investigar.setMovimientoInvestigar(
+      idArchivoOrigen2, origen,nombreArchivo,fechaDeTransaccion,responsable,descripcion,movRef);
 
+    Test_20181009113614_create_sp_crea_movimiento_investigar.setMovimientoInvestigar(
+      idArchivoOrigen3, origen,nombreArchivo,fechaDeTransaccion,responsable,descripcion,movRef);
+
+    Map<String, Object> resp = searchMovimientoInvestigar(idArchivoOrigenNull);
     List results = (List)resp.get("result");
     Assert.assertNotNull("Debe existir", results);
     Assert.assertEquals("Deben encontrar 3 movimientos", 3, results.size());
+
   }
 
   @Test
   public void testBuscaMovimientosAInvestigar_otherId() throws SQLException {
 
-    Test_20181009113614_create_sp_crea_movimiento_investigar.creaMovimientoInvestigar("idMov=10", "Testing", "archivo1.txt");
+    String idArchivoOrigen = "idMov=10";
+    String origen = "Testing";
+    String nombreArchivo = "archivo.txt";
+    Timestamp fechaDeTransaccion = new Timestamp((new Date()).getTime());
+    String responsable = "Conciliaciones Prepago";
+    String descripcion = "Movimiento no encontrado en archivo";
+    Long movRef = new Long(0);
 
-    Object[] params = {
-      new InParam("otro", Types.VARCHAR)
-    };
+    Test_20181009113614_create_sp_crea_movimiento_investigar.setMovimientoInvestigar(
+      idArchivoOrigen, origen,nombreArchivo,fechaDeTransaccion,responsable,descripcion,movRef);
 
-    RowMapper rm = (Map<String, Object> row) -> {
-      ResearchMovement researchMovement = new ResearchMovement();
-      researchMovement.id = numberUtils.toLong(row.get("_id"));
-      researchMovement.idRef = String.valueOf(row.get("_mov_ref"));
-      researchMovement.origin = String.valueOf(row.get("_origen"));
-      researchMovement.fileName = String.valueOf(row.get("_nombre_archivo"));
-      researchMovement.createdAt = (Timestamp) row.get("_fecha_registro");
-      return researchMovement;
-    };
-
-    Map<String, Object> resp = dbUtils.execute(SP_NAME, rm, params);
-
+    Map<String, Object> resp = searchMovimientoInvestigar("otro");
     List results = (List)resp.get("result");
     Assert.assertNull("No debe existir", results);
   }
 
+
   class ResearchMovement {
     private Long id;
-    private String idRef;
-    private String fileName;
-    private String origin;
-    private Timestamp createdAt;
+    //private String idRef;
+    private String idArchivoOrigen;
+    private String origen;
+    private String nombreArchivo;
+    private Timestamp fechaRegistro;
+    private Timestamp fechaDeTransaccion;
+    private String responsable;
+    private String descripcion;
+    private Long movRef;
   }
 }
 
