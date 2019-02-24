e0f2d41871973666ada4a886d42749acc3f627a3
mauricio-valencia <mauricio.valencia@multicaja.cl>
2019-02-24 09:15:11 -0300

    Fix: Cambios en Tests

--- pg/src/test/java/cl/multicaja/test/db/Test_20181009113614_create_sp_crea_movimiento_investigar.java
+++ pg/src/test/java/cl/multicaja/test/db/Test_20181009113614_create_sp_crea_movimiento_investigar.java
@@ -25,7 +25,6 @@ public class Test_20181009113614_create_sp_crea_movimiento_investigar extends Te
   @AfterClass
   public static void afterClass() {
     dbUtils.getJdbcTemplate().execute(String.format("DELETE FROM %s.prp_movimiento_investigar", SCHEMA));
-
   }
 
 
