d85c35c637518b1fe9b46fef209167657b8baae3
mauricio-valencia <mauricio.valencia@multicaja.cl>
2019-03-13 19:06:34 -0300

    Fix Tests

--- app/src/test/java/cl/multicaja/test/integration/v10/async/Test_PrepaidMovementEJB10_fullClearingResolution.java
+++ app/src/test/java/cl/multicaja/test/integration/v10/async/Test_PrepaidMovementEJB10_fullClearingResolution.java
@@ -451,6 +451,7 @@ public class Test_PrepaidMovementEJB10_fullClearingResolution extends TestBaseUn
         getResearchMovementByMovRef(NumberUtils.getInstance().toBigDecimal(movementRejected.prepaidMovement10.getId()));
       Assert.assertEquals("Debe estar en research",1, researchMovements.size());
       //Assert.assertEquals("Debe venir del clearing", ReconciliationOriginType.CLEARING_RESOLUTION, researchMovements.get(0).getOriginType());
+      
     }
 
     // 8. Chequea test: Es RETIRO + Es WEB + OK Tecnocom + NO Conciliado en BD + MovStatus: process OK + >> Clearing ya estaba OK
