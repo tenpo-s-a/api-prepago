833d9185fc58aba35470e141ec3ce4dbb269787c
JOG <orellana.gourdet@gmail.com>
2019-03-27 09:19:05 -0300

    fix

--- app/src/main/java/cl/multicaja/prepaid/dao/CuentaDao.java
+++ app/src/main/java/cl/multicaja/prepaid/dao/CuentaDao.java
@@ -8,11 +8,13 @@ import javax.persistence.PersistenceContext;
 
 public class CuentaDao extends PrepaidCrud<Cuenta,Long>{
 
+  private static final String ENV = "dev";
+
   public CuentaDao() {
     super(Cuenta.class);
   }
 
-  @PersistenceContext
+  @PersistenceContext(name = ENV)
   private EntityManager em;
 
   @Override
