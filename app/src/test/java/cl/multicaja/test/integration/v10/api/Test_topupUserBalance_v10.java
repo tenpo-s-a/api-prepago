9082e42f4ad8a0e71aa9f1c11af15f7387cd4ce2
Agustin Antonissen <agustin.antonissen@multicaja.cl>
2019-04-08 09:11:02 -0400

    fix truncate

--- app/src/test/java/cl/multicaja/test/integration/v10/api/Test_topupUserBalance_v10.java
+++ app/src/test/java/cl/multicaja/test/integration/v10/api/Test_topupUserBalance_v10.java
@@ -30,9 +30,9 @@ public class Test_topupUserBalance_v10 extends TestBaseUnitApi {
   @Before
   @After
   public void clearData() {
-    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE FROM %s.clearing CASCADE", getSchemaAccounting()));
-    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE FROM %s.accounting CASCADE", getSchemaAccounting()));
-    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE FROM %s.prp_movimiento CASCADE", getSchema()));
+    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE TABLE %s.clearing CASCADE", getSchemaAccounting()));
+    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE TABLE %s.accounting CASCADE", getSchemaAccounting()));
+    getDbUtils().getJdbcTemplate().execute(String.format("TRUNCATE TABLE %s.prp_movimiento CASCADE", getSchema()));
   }
 
   @Test
