e1434a917f943567688ded8ece6bb83590d9ebac
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2019-04-18 14:11:28 -0400

    Elimina kong del proyecto

--- kong/migrate-new.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean compile
-mvn exec:java -Dexec.mainClass="cl.multicaja.core.kong.CreateMigration"
--- kong/src/test/java/cl/multicaja/test/kong/Test_20181204103914.java
+++
@@ -1,9 +0,0 @@
-package cl.multicaja.test.kong;
-
-import cl.multicaja.core.test.TestKongBase;
-import org.junit.Assert;
-import org.junit.Test;
-
-public class Test_20181204103914 extends TestKongBase {
-
-}
